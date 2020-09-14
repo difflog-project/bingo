1109d0880470b1589896decbf6b4d4acb87cc6d8
Tianyi Chen <TianyiChen@users.noreply.github.com>
2020-09-14 09:21:57 -0700

    fix dfs weight

--- scripts/bnet/rankv2.py
+++ scripts/bnet/rankv2.py
@@ -9,17 +9,6 @@ def du2node(x):
 	if x not in DU2node:
 		DU2node[x]=ug.add_vertex()
 	return DU2node[x]
-def load():
-	global DU2node,ug
-	if ncons is None:return
-	i=0
-	for x in open(ncons).readlines():
-		x=x.split(': ')[1].split(', ')
-		b=x[-1][:-1]
-		for a in x[:-1]:
-			ug.add_edge(du2node(a[4:]),du2node(b))
-	global comp,hist
-	comp,hist=label_components(ug)
 def dfs(v,depth=0):
 	global visited
 	visited.add(v)
@@ -29,11 +18,28 @@ def dfs(v,depth=0):
 			if x not in visited:
 				rt+=dfs(x,depth+1)
 	return rt
+weight_cache={}
+def load():
+	global DU2node,ug
+	if ncons is None:return
+	i=0
+	for x in open(ncons).readlines():
+		x=x.split(': ')[1].split(', ')
+		b=x[-1][:-1]
+		for a in x[:-1]:
+			ug.add_edge(du2node(a[4:]),du2node(b))
+	global comp,hist,visited,weight_cache
+	comp,hist=label_components(ug)
+	for x in DU2node:
+		if x.startswith('Alarm'):
+			visited=set()
+			weight_cache[x]=dfs(DU2node[x])
+	mw=max(weight_cache.values())
+	for x in weight_cache:
+		weight_cache[x]=.1*weight_cache[x]/mw
 
 def get_weight(n):
-	global visited
-	visited=set()
 	# weight in [0,1]
 	if ncons is None:return 1
-	return .1*dfs(DU2node[n])
+	return .1*weight_cache[n]
 	#return .2*hist[comp[DU2node[n]]]/max(hist)
