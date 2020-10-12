93c9980ea89c407cc0ee63341fabfccfe3b0b8e1
Tianyi Chen <>
2020-10-11 22:28:36 -0700

    conforms original name tie breaking

--- scripts/bnet/rankv2.py
+++ scripts/bnet/rankv2.py
@@ -6,6 +6,8 @@ def load():
 	global ptprob
 	for x in open(PT).readlines():
 		x=x.split()
-		ptprob[x[-1]]=float(x[1])
+		# Assuming init prob not too small
+		if '0.' in x[1]:
+			ptprob[x[-1]]=float(x[1])
 def get_weight(n):
 	return ptprob[n] if n in ptprob else 0
